080be41ca139c50009be1c65ad421f7f5d1d7b06
Anya A M <anmoh23@student.sdu.dk>
2025-11-04 15:27:01 +0100

    Alberte's current changes! :)

---
+++ app/src/main/java/com/example/moneytalks/NavigationBar.kt
@@ -0,0 +1,15 @@
+package com.example.moneytalks
+
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+
+
+@Composable
+fun NavBar(modifier: Modifier = Modifier) {
+    //val navController = rememberNavController()
+    //val startDestination = Dest
+
+}
