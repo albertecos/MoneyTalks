d74d6da87463c09ef15bbfbf51b371b698c1043e
Anya A M <anmoh23@student.sdu.dk>
2025-11-06 10:05:52 +0100

    Current GroupView - The skeleton for the page is almost done >:) - Colors are also added

--- app/src/main/java/com/example/moneytalks/ui/theme/Color.kt
+++ app/src/main/java/com/example/moneytalks/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.example.moneytalks.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+//The old colors
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +10,14 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightBlue = Color(0xFFBADFFF)
+val DarkBlue = Color(0xFF3C4780)
+val GreyColor = Color(0xF2F2F2F2)
+
+val DarkGrey = Color(0xF28D8D8D)
+
+//Balance score
+val redInDebt = Color(0xFFAC0000)
+val greenCreditor = Color(0xFF007F13)
+val blueDebtFree = Color(0xFF3F92DA)
