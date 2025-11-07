50851f75add0da4a26c3614f5cca7fe6870cec5e
albertecos <144015396+albertecos@users.noreply.github.com>
2025-11-07 16:23:55 +0100

    small filestructure fix

--- app/src/main/java/com/example/moneytalks/Popup/LeaveGroupPopup.kt
+++ app/src/main/java/com/example/moneytalks/Popup/LeaveGroupPopup.kt
@@ -1,31 +1,23 @@
-package com.example.moneytalks.Cards
+package com.example.moneytalks.Popup
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -38,8 +30,6 @@ import com.example.moneytalks.ui.theme.DarkBlue
 import com.example.moneytalks.ui.theme.LightLightBlue
 import com.example.moneytalks.ui.theme.greenCreditor
 import com.example.moneytalks.ui.theme.redInDebt
-import java.util.Properties
-import kotlin.toString
 
 @Composable
 fun ShowLeavePopup(
