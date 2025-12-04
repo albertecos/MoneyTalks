549bcaa5f50821c8f162e7a2853ed8711ec732df
Anya A M <anmoh23@student.sdu.dk>
2025-12-04 15:42:35 +0100

    Logo added instead of text

--- app/src/main/java/com/example/moneytalks/bars/HeaderBar.kt
+++ app/src/main/java/com/example/moneytalks/bars/HeaderBar.kt
@@ -1,7 +1,10 @@
 package com.example.moneytalks.bars
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
@@ -14,13 +17,17 @@ import androidx.compose.ui.text.style.TextOverflow
 import com.example.moneytalks.ui.theme.LilyScriptOne
 import androidx.compose.material3.Icon
 import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.moneytalks.navigation.Destination
 import com.example.moneytalks.navigation.NavIcon
 import com.example.moneytalks.ui.theme.Black
 import com.example.moneytalks.ui.theme.DarkBlue
 import com.example.moneytalks.ui.theme.gradient
-
+import com.example.moneytalks.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -34,11 +41,11 @@ fun TopBar(navController: NavController, scrollBehavior: TopAppBarScrollBehavior
             titleContentColor = Black
         ),
         title = {
-            Text(
-                text = "Money Talk",
-                fontFamily = LilyScriptOne,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
+            Image(
+                painter = painterResource(R.drawable.mtlogo),
+                "Logo",
+                modifier = Modifier
+                    .size(90.dp)
             )
         },
         scrollBehavior = scrollBehavior,
